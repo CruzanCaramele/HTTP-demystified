e6559fe6880930755fb51f3d734b7f01605fbcee
CruzanCaramele <letters2hamza@gmail.com>
2015-03-07 15:54:15 +0800

    Perform Create operation on databse by adding POST for new restaurants

--- webserver.py
+++ webserver.py
@@ -32,6 +32,9 @@ class webServerHandler(BaseHTTPRequestHandler):
 				output = ""
 				output += "<html><body>"
 				output += "<h1>Restaurants</h1>"
+				output += "<h3>\
+				    	     <a href='restaurants/new'>Add A new Restaurant</a>\
+					    </h3>"
 
 				for restaurant in restaurants:
 					output += restaurant.name
@@ -46,12 +49,61 @@ class webServerHandler(BaseHTTPRequestHandler):
 				print output
 				return
 
+
+			if self.path.endswith("/restaurants/new"):
+				output = ""
+				self.send_response(200)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+
+				output += "<html><body>"
+				output += "<h1>New Restaurant Name</h1>"
+				output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
+				output += "<input name='newRestaurantName' type='text'>"
+				output += "<input type='submit' value='Create'>"
+				output += "</form>"
+				output += "</body></html>"
+
+				self.wfile.write(output)
+				print output
+				return
+
+
+
 		except IOError:
 			self.send_error(404, "File Not Found %s" % self.path)
 
 
 	def do_POST(self):
-		pass
+		
+		try:
+			if self.path.endswith("/restaurants/new"):
+
+				ctype, pdict = cgi.parse_header(self.headers.getheader("content-type"))
+
+				#check of content-type is form
+				if ctype == "multipart/form-data":
+
+					#collect all fields from form, fields is a dictionary
+					fields = cgi.parse_multipart(self.rfile, pdict)
+
+					#extract the name of the restaurant from the form
+					messagecontent = fields.get("newRestaurantName")
+
+					newRestaurant = Restaurant(name=messagecontent[0])
+					session.add(newRestaurant)
+					session.commit()
+
+					#response headers
+					self.send_response(301)
+					self.send_header("Content-type", "text/html")
+					self.send_header('Location','/restaurants')
+					self.end_headers()
+
+
+
+		except:
+			pass
 
 
 
